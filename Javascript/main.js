6ff8912a4918234316abc73d131898eb0faace0e
Rayhan Abdullah <programmer.rayhan1@gmail.com>
2025-03-25 22:06:21 +0600

    practice promise

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,11 +1,47 @@
-function fetchData(callback) {
+function getUserData(callback) {
     setTimeout(() => {
-        callback("Data received");
-    }, 1000);
+        callback(null, { id: 10, name: "Rayhan Abdullah" });
+    }, 2000);
 }
 
-fetchData((result) => {
-    console.log(result);
+getUserData((error, data) => {
+    if (error) {
+        console.log("Error:", error);
+    } else {
+        console.log("User Data:", data);
+    }
 });
 
-console.log("Hello, world!");
+// using promise
+function getUserData1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ id: 1, name: "Rayhan Abdullah" });
+        }, 1000);
+    });
+}
+
+getUserData1()
+    .then((data) => console.log("User Data:", data))
+    .catch((error) => console.log("Error:", error));
+
+// using async await
+function getUserData2() {
+    throw new Error("Something went wrong");
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ id: 111, name: "Rayhan Abdullah" });
+        }, 3000);
+    });
+}
+
+async function fetchData2() {
+    try {
+        const data = await getUserData2();
+        console.log("User Data:", data);
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
+}
+
+fetchData2();
