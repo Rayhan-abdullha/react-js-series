737a8d8aae4fd95baff4711514b97903fdbffced
Rayhan Abdullah <programmer.rayhan1@gmail.com>
2025-03-25 14:35:01 +0600

    redux

---
+++ redux-tolkits/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { increment } from './store/slice/counter'
+
+const App = () => {
+  const dispatch = useDispatch()
+  const {count} = useSelector((state) => state.counter)
+  const handleClick = () => {
+    dispatch(increment())
+  }
+  return (
+    <div>
+      <h1>Counter: { count}</h1>
+      <button className='px-4 py-2 bg-slate-700 text-white cursor-pointer rounded-md' onClick={handleClick}>counter</button>
+    </div>
+  )
+}
+
+export default App
